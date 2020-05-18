9865826f55bf0800cc20943b1dd5a407a7983b2d
dleonhardt <dleonhardt01@gmail.com>
2020-05-18 09:18:49 -0400

    More fixes found from QA testing.

--- 01-Progressing/04-Props/Unsolved/src/components/Nav/Nav.js
+++ 01-Progressing/04-Props/Unsolved/src/components/Nav/Nav.js
@@ -9,14 +9,14 @@ function Nav() {
 
 	return (
 		<nav className="container">
-			{/* TODO: Add handleScroll to John Doe (HINT: onClick={() => handleScroll(0)}) */}
+			{/* TODO: Add handleScroll to John Doe (HINT: handleScroll(0) will take you to the top of the page) */}
 			<h1><button>John Doe</button></h1>
 			<ul>
-				{/* TODO: Add handleLink through props for each button below (HINT: onClick={() => handleLink("https://google.com/")}) */}
+				{/* TODO: Add handleLink through props as a onClick event for each button below (HINT: handleLink("https://google.com/") */}
 				<li><button>Resume</button></li>
 				<li><button>GitHub</button></li>
 				<li><button>LinkedIn</button></li>
-				{/* TODO: Add handleScroll to Contact similar to how it was done for John Doe */}
+				{/* TODO: Add handleScroll to Contact similar to how it was done for John Doe (HINT: document.body.scrollHeight will take you to the bottom of the document) */}
 				<li><button>Contact</button></li>
 			</ul>
 		</nav>
